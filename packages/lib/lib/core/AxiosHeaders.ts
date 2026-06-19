91de129a66566a4f295705e507af30a9c8a7b4d2
Adrian Elton-Browning <adrian.browning@tealium.com>
2026-06-19 13:05:09 +0000

    fix(AxiosHeaders): lint errors in AxiosHeaders.ts

--- packages/lib/lib/core/AxiosHeaders.ts
+++ packages/lib/lib/core/AxiosHeaders.ts
@@ -26,11 +26,13 @@ function normalizeHeader(header: string): string {
 function normalizeValue(
   value: AxiosHeaderValue | undefined
 ): AxiosHeaderValue | undefined {
-  return value === false || value == null
-    ? value
-    : utils.isArray(value)
-      ? value.map(v => normalizeValue(v) as string)
-      : sanitizeHeaderValue(String(value)) as string;
+  if (value === false || value == null) {
+    return value;
+  }
+  if (utils.isArray(value)) {
+    return value.map(v => normalizeValue(v) as string);
+  }
+  return sanitizeHeaderValue(String(value));
 }
 
 function parseTokens(str: string): Record<string, string> {
