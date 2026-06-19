cd29ec5876ee4e5fa17dcc73c318e60e883584b5
Adrian Elton-Browning <adrian.browning@tealium.com>
2026-06-19 14:05:26 +0000

    fix(toURLEncodedForm): lint errors in toURLEncodedForm.ts

--- packages/lib/lib/helpers/toURLEncodedForm.ts
+++ packages/lib/lib/helpers/toURLEncodedForm.ts
@@ -1,12 +1,12 @@
 "use strict";
 
 import platform from "../platform/index.js";
-import type { FormDataVisitorHelpers } from "../types.js";
+import type { FormDataVisitorHelpers, GenericFormData } from "../types.js";
 import utils from "../utils.js";
 import toFormData from "./toFormData.js";
 
 export default function toURLEncodedForm(data: unknown, options?: Record<string, unknown>) {
-  return toFormData(data, new platform.classes.URLSearchParams() as unknown as import("../types.js").GenericFormData, {
+  return toFormData(data, new platform.classes.URLSearchParams() as unknown as GenericFormData, {
     visitor: function (this: { append: (name: string, value: unknown) => void; }, value: unknown, key: string | number, path: null | Array<string | number>, helpers: FormDataVisitorHelpers) {
       if (platform.isNode && utils.isBuffer(value)) {
         this.append(String(key), (value as Buffer).toString("base64"));
