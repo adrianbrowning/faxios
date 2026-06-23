4cdc9261b7d2affa546636ef677c0d493089de73
Adrian Elton-Browning <adrian.browning@tealium.com>
2026-06-23 06:59:15 +0000

    fix(tests/unit/helpers): ts errors in isAxiosError.test.ts

--- packages/lib/tests/unit/helpers/isAxiosError.test.ts
+++ packages/lib/tests/unit/helpers/isAxiosError.test.ts
@@ -4,14 +4,14 @@ import isAxiosError from "../../../src/lib/helpers/isAxiosError.js";
 
 describe("helpers::isAxiosError", () => {
   it("should return true if the error is created by core::createError", () => {
-    expect(isAxiosError(new AxiosError("Boom!", null, { foo: "bar" }))).toBe(
+    expect(isAxiosError(new AxiosError("Boom!", undefined, { foo: "bar" }))).toBe(
       true,
     );
   });
 
   it("should return true if the error is enhanced by core::enhanceError", () => {
     expect(
-      isAxiosError(AxiosError.from(new Error("Boom!"), null, { foo: "bar" })),
+      isAxiosError(AxiosError.from(new Error("Boom!"), undefined, { foo: "bar" })),
     ).toBe(true);
   });
 
