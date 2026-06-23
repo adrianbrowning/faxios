586611d338a0a698ae16cca38bbd4f4c4151f636
Adrian Elton-Browning <adrian.browning@tealium.com>
2026-06-23 00:51:51 +0000

    fix(tests/unit/helpers): ts errors in bind.test.ts

--- packages/lib/src/lib/helpers/bind.ts
+++ packages/lib/src/lib/helpers/bind.ts
@@ -7,8 +7,8 @@
  * @param {*} thisArg - The value to be passed as the `this` parameter
  * @returns {Function} A new function that will call the original function with the specified `this` context
  */
-export default function bind(fn: (...args: Array<unknown>) => unknown, thisArg: unknown) {
-  return function wrap(...args: Array<unknown>) {
-    return fn.apply(thisArg, args);
+export default function bind<T extends (...args: never[]) => unknown>(fn: T, thisArg: unknown): (...args: Parameters<T>) => ReturnType<T> {
+  return function wrap(...args: Parameters<T>): ReturnType<T> {
+    return fn.apply(thisArg, args) as ReturnType<T>;
   };
 }
