1e80d012637342352797d48968b746f9f125ccbc
Adrian Elton-Browning <adrian.browning@tealium.com>
2026-06-24 19:24:00 +0100

    push

--- packages/lib/src/lib/adapters/fetch.ts
+++ packages/lib/src/lib/adapters/fetch.ts
@@ -117,7 +117,8 @@ const maybeWithAuthCredentials = (url: string): boolean => {
 
 // eslint-disable-next-line sonarjs/function-return-type
 const factory = (env: Record<string, unknown>) => {
-  const globalObject: Record<string, unknown> = utils.global ?? globalThis as unknown as Record<string, unknown>;
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  const globalObject: Record<string, unknown> = utils.global ?? globalThis;
   const ReadableStream = globalObject["ReadableStream"] as
     | (AnyConstructor & { prototype: AnyReadableStream; })
     | undefined;
