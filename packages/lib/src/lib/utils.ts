8c67e78f20ce5d36a953038bd20266c2bec851ff
Adrian Elton-Browning <adrian.browning@tealium.com>
2026-06-24 09:18:07 +0100

    chore: TS/Lint Fixes

--- packages/lib/src/lib/utils.ts
+++ packages/lib/src/lib/utils.ts
@@ -308,7 +308,7 @@ function getGlobal(): Record<string, unknown> {
   // @ts-expect-error window may not exist in all environments
    
   if (typeof window !== "undefined") return window;
-  if (typeof global !== "undefined") return global as unknown as Record<string, unknown>;
+  if (typeof global !== "undefined") return global;
   return {};
 }
 
