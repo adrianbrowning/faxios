8019db9edb8349aac6ce25408c4b4fc481753f69
Adrian Elton-Browning <adrian.browning@tealium.com>
2026-07-06 11:43:34 +0100

    chore: Formatting prompt

--- .sandcastle/review.ts
+++ .sandcastle/review.ts
@@ -9,7 +9,9 @@ await using sandbox = await createSandbox({
 
 await sandbox.run({
   agent: claudeCode("us.anthropic.claude-opus-4-6-v1"),
-  prompt: "Use the skill /cc-pr-review Review the changes in this pull request. Write to a .md file and .html file under the local ./.claude/reviews folder.\n When complete emit <promise>COMPLETE</promise>",
+  prompt: `Use the skill /cc-pr-review Review the changes in this pull request. Write to a .md file and .html file under the local ./.claude/reviews folder.
+Ignore .sandcastle & plan dirs
+When complete emit <promise>COMPLETE</promise>`,
     maxIterations: 3,
 });
 
@@ -17,6 +19,10 @@ const found = await sandbox.exec(`find ${sandbox.worktreePath}/.claude/reviews -
 
 await sandbox.run({
   agent: claudeCode("us.anthropic.claude-opus-4-6-v1"),
-  prompt: `Validate this review file: ${found.stdout}\nCheck completeness, and that findings reference real code.\n output your final review into the .claude/reviews dir.\n When complete emit <promise>COMPLETE</promise>`,
+  prompt: `Validate this review file: ${found.stdout}
+Check completeness, and that findings reference real code.
+Output your final review into the .claude/reviews dir.
+Ignore .sandcastle & plan dirs
+When complete emit <promise>COMPLETE</promise>`,
     maxIterations: 2
 });
